0cae8dc81f0e08ee9450f5635b37a10a0bcd6cb3
Marlo Eichenberg Motta <devmaster@programmerhero.com.br>
2022-08-27 19:31:51 -0300

    feat: adicionado método com ponteiro para struct

--- 15. Métodos/metodos.go
+++ 15. Métodos/metodos.go
@@ -8,22 +8,29 @@ type usuario struct {
 }
 
 func (u usuario) salvar() {
-	fmt.Printf("Salvando os dados do Usuário '%s' no banco de dados", u.nome)
+	fmt.Printf("Salvando os dados do Usuário '%s' no banco de dados\n", u.nome)
 }
 
 func (u usuario) maiorDeIdade() bool {
 	return u.idade >= 18
 }
 
+func (u *usuario) fazerAniversario() {
+	u.idade++
+}
+
 func main() {
 	fmt.Println("Métodos")
 
 	usuario1 := usuario{"John Doe", 21}
-	fmt.Println(usuario1)
 	usuario1.salvar()
 
 	usuario2 := usuario{"Willy Wonka", 55}
-	fmt.Println(usuario2)
 	usuario2.salvar()
 
+	maiorDeIdade := usuario2.maiorDeIdade()
+	fmt.Println(maiorDeIdade)
+
+	usuario2.fazerAniversario()
+	fmt.Println(usuario2.idade)
 }
