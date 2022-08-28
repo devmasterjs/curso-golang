85267380aa798564e47f76208aad7bc36c5e07ad
Marlo Eichenberg Motta <devmaster@programmerhero.com.br>
2022-08-27 23:56:18 -0300

    feat: return Server Names from host

--- projetos/terminal/go-server-info/app/app.go
+++ projetos/terminal/go-server-info/app/app.go
@@ -14,17 +14,25 @@ func GoServerInfo() *cli.App {
 	app.Name = "Aplicação de linha de comando"
 	app.Usage = "Busca IPs e Nomes de Servidores na Internet"
 
+	flags := []cli.Flag{
+		cli.StringFlag{
+			Name:  "host",
+			Value: "github.com",
+		},
+	}
+
 	app.Commands = []cli.Command{
 		{
-			Name:  "ip",
-			Usage: "Busca IPs de endereços na internet",
-			Flags: []cli.Flag{
-				cli.StringFlag{
-					Name:  "host",
-					Value: "github.com",
-				},
-			},
+			Name:   "ip",
+			Usage:  "Busca IPs de endereços na internet",
+			Flags:  flags,
 			Action: getIps,
+		},
+		{
+			Name:   "server",
+			Usage:  "Busa o nome dos servidores na internet",
+			Flags:  flags,
+			Action: getServerNames,
 		},
 	}
 
@@ -41,6 +49,20 @@ func getIps(c *cli.Context) {
 	}
 
 	for _, ip := range ips {
-		fmt.Println(ip)
+		fmt.Println("IP:", ip)
+	}
+}
+
+// Busca Nomes dos Servidoes na Internet
+func getServerNames(c *cli.Context) {
+	host := c.String("host")
+
+	servers, erro := net.LookupNS(host)
+	if erro != nil {
+		log.Fatal(erro)
+	}
+
+	for _, server := range servers {
+		fmt.Println("Server:", server.Host)
 	}
 }
