7cd2e5be2c4ef10d781ba8cb7be24b6f0f9613d7
Marlo Eichenberg Motta <devmaster@programmerhero.com.br>
2022-08-27 23:36:47 -0300

    feat: return IPs from host

--- projetos/terminal/go-server-info/app/app.go
+++ projetos/terminal/go-server-info/app/app.go
@@ -1,11 +1,46 @@
 package app
 
-import "github.com/urfave/cli"
+import (
+	"fmt"
+	"log"
+	"net"
+
+	"github.com/urfave/cli"
+)
 
 // Aplicação de linha de comando
 func GoServerInfo() *cli.App {
 	app := cli.NewApp()
 	app.Name = "Aplicação de linha de comando"
 	app.Usage = "Busca IPs e Nomes de Servidores na Internet"
+
+	app.Commands = []cli.Command{
+		{
+			Name:  "ip",
+			Usage: "Busca IPs de endereços na internet",
+			Flags: []cli.Flag{
+				cli.StringFlag{
+					Name:  "host",
+					Value: "github.com",
+				},
+			},
+			Action: getIps,
+		},
+	}
+
 	return app
+}
+
+// Busca IPs na internet
+func getIps(c *cli.Context) {
+	host := c.String("host")
+
+	ips, erro := net.LookupIP(host)
+	if erro != nil {
+		log.Fatal(erro)
+	}
+
+	for _, ip := range ips {
+		fmt.Println(ip)
+	}
 }
